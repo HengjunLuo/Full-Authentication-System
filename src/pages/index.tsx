53d7c97b0db996be791aeb4da475887bb9188705
Hengjun <hengjunluo@gmail.com>
2023-06-20 17:15:32 -0600

    Adjusting styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { NextPageContext } from "next";
 import { useSession, signIn, signOut, getSession } from "next-auth/react"
+import {FaGithub} from "react-icons/fa"
 export default function Home() {
   const { data: session } = useSession();
   console.log(session);
 
   return (
     <div className="bg-black min-h-screen text-white flex items-center justify-center">
-      <div className="mx-auto">
+      <div className="container mx-auto">
         <div className="border border-white relative flex flex-col w-full rounded-lg">
           <div className="flex flex-wrap justify-center items-center">
             <div className="w-full text-right">
@@ -23,6 +24,20 @@ export default function Home() {
               <div className="tetx-sm mb-2 font-bold">{session?.user?.email}</div>
               <div className="mb-2 mt-10"> You Logged in using &nbsp;
                 <span className="capitalize bg-blue-400 px-4 py-1 ml-2 font-bld italix text-lg rounded-md">{session?.user?.provider}</span></div>
+            </div>
+          </div>
+          <div className="mt-10 py-10 border-t text-center">
+            <div className="flex flex-wrap justify-center">
+                <div className="w-full px-4">
+                  <p className="mb-4 text-sm">This is an Authentication System with full functionalities.</p>
+                  <p className="font-bold text-xs">This project is built using Next.js NextAuth.js ...</p>
+                  <div className="mt-6 flex items-center justify-center gap2">
+                    Source Code Here: &nbsp;
+                    <a href="https://github.com/HengjunLuo/Full-Authentication-System" target="_blank" rel="noopener norefresher" className="text-4xl">
+                    <FaGithub/>
+                    </a>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
