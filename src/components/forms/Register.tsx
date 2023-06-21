ed5af2558ebb205949cf77669d496f63564cfe64
Hengjun <hengjunluo@gmail.com>
2023-06-21 16:43:10 -0600

    Adding more inputs to register form, and adding validation to those inputs

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,17 +1,47 @@
 import * as React from 'react';
 import Input from '../inputs/Input';
-import {AiOutlineUser} from "react-icons/ai"
+import { AiOutlineUser, AiOutlineMail, AiOutlinePhone, AiOutlineLock } from "react-icons/ai";
+import { useForm } from 'react-hook-form';
+import { z } from "zod";
+import validator from 'validator';
+import { zodResolver } from "@hookform/resolvers/zod";
+interface IRegisterformProps { }
 
-interface IRegisterformProps {
-}
-
+const FormSchema = z.object({
+  first_name: z.string().min(2, "First name must be between 2-32 characters")
+    .max(32, "First name must be between 2-32 characters")
+    .regex(new RegExp("^[a-zA-Z]+$"), "No special characters allowed"),
+  last_name: z.string().min(2, "Last name must be between 2-32 characters")
+    .max(32, "Last name must be between 2-32 characters")
+    .regex(new RegExp("^[a-zA-Z]+$"), "No special characters allowed"),
+  email: z.string().email('Please enter a valid email address'),
+  phone: z.string().refine(validator.isMobilePhone, {
+    message: "Please enter a valid phone number"
+  }),
+  password: z.string().min(8, "Password must be between 8~64 characters")
+    .max(64, "Password must be between 8~64 characters"),
+  confirmPassword: z.string(),
+})
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password unmatch!",
+    path: ['confirmPassword'],
+  });
+type FormSchemaType = z.infer<typeof FormSchema>;
 const Registerform: React.FunctionComponent<IRegisterformProps> = (props) => {
+  const { register, handleSubmit, watch, formState: { errors, isSubmitting } } = useForm<FormSchemaType>({ resolver: zodResolver(FormSchema) });
+  const onSubmit = (data: any) => console.log(data);
   return (
-  <form className='my-8 text-sm'>
-    <div className='gap-2 md:flex'>
-        <Input name="first_name" label="First_name" type="text" icon={<AiOutlineUser/>} placeholder="example"></Input>
-    </div>
-  </form>);
+    <form className='my-8 text-sm' onSubmit={handleSubmit(onSubmit)}>
+      <div className='gap-2 md:flex flex-col'>
+        <Input name="first_name" label="First name" type="text" icon={<AiOutlineUser />} placeholder="Enter first name here" register={register} error={errors?.first_name?.message} disabled={isSubmitting}></Input>
+        <Input name="last_name" label="Last name" type="text" icon={<AiOutlineUser />} placeholder="Enter last name here" register={register} error={errors?.last_name?.message} disabled={isSubmitting}></Input>
+        <Input name="email" label="Email address" type="text" icon={<AiOutlineMail />} placeholder="abcd@efg.com" register={register} error={errors?.email?.message} disabled={isSubmitting}></Input>
+        <Input name="phone" label="Phone number" type="text" icon={<AiOutlinePhone />} placeholder="+(xxx) xxx-xxx-xxx" register={register} error={errors?.phone?.message} disabled={isSubmitting}></Input>
+        <Input name="password" label="Password" type="text" icon={<AiOutlineLock />} placeholder="Enter password here" register={register} error={errors?.password?.message} disabled={isSubmitting}></Input>
+        <Input name="confirmPassword" label="Confirm Password" type="text" icon={<AiOutlineLock />} placeholder="Re-enter password here" register={register} error={errors?.confirmPassword?.message} disabled={isSubmitting}></Input>
+      </div>
+      <button type='submit'>Submit</button>
+    </form>);
 };
 
 export default Registerform;
--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,25 +1,46 @@
 import * as React from 'react';
-
+import { IoAlertCircle } from 'react-icons/io5'
 interface IInputProps {
     name: string;
     label: string;
     type: string;
     icon: JSX.Element;
     placeholder: string;
+    register: any;
+    error: any;
+    disabled: boolean;
 }
 
 const Input: React.FunctionComponent<IInputProps> = (props) => {
-    const { name, label, type, icon, placeholder } = props;
+    const { name, label, type, icon, placeholder, register, error, disabled } = props;
     return (
-        <div className='relative mt-1 rouned-md shadow-sm'>
-            <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
-                <span className="text-gray-500 text-sm">{icon}</span>
+        <div className='mt-3 w-[100%]'>
+            <label htmlFor={name} className='text-gray-700'>{label}</label>
+            <div className='relative mt-1 rouned-md shadow-sm'>
+                <div className="pointer-event-none absolute left-0 top-0.5 inset-y-0 flex items-center pl-3"
+                    style={{ transform: `${error ? "translateY(-12px)" : ""}` }}>
+                    <span className="text-gray-500 text-sm">{icon}</span>
+                </div>
+                <input
+                    type={type}
+                    className='w-full py-2 pr-7 pl-8 block rounded-md border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm'
+                    placeholder={placeholder}
+                    {...register(name)}
+                    style={{
+                        border: "solid",
+                        borderColor: `${error ? "#ED4337" : ""}`,
+                    }}
+                />
+                {
+                    error &&
+                    (
+                        <div className='fill-red-500 absolute right-2 top-2.5 text-xl'>
+                            <IoAlertCircle fill='#ED4337' />
+                        </div>
+                    )
+                }
+                {error && <p className='text-sm text-[#ED4337] mt-1'>{error}</p>}
             </div>
-            <input
-                type={type}
-                className='w-full py-2 pr-7 pl-8 block rounded-md border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm'
-                placeholder={placeholder}
-            />
         </div>);
 };
 
