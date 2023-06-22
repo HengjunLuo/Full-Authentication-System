5e34981477c758b41586265207aed63da4495077
Hengjun <hengjunluo@gmail.com>
2023-06-21 18:36:16 -0600

    Fixing auth page backgound image path

--- src/components/Backgrounds/Background.tsx
+++ src/components/Backgrounds/Background.tsx
@@ -5,7 +5,7 @@ interface IBackgroundProps {
 
 const Background: React.FunctionComponent<IBackgroundProps> = (props) => {
   return <div className=' min-h-screen lg:flex lg:w-1/2 xl:w-2/3 2xwl:w-3/4 bg-contain bg-no-repeat bg-center'
-  style={{backgroundImage: `url("../public/favicon.ico`}}></div>;
+  style={{backgroundImage: `url("../../auth/auth.jpg`}}></div>;
 };
 
 export default Background;
